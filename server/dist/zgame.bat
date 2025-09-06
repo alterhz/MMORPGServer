4b013cbe1c8f75aa352f89dafd55e05bcb0d8778
alterhz <550360139@qq.com>
2025-09-06 12:42:42 +0800

    设置utf-8编码

--- server/dist/zgame.bat
+++ server/dist/zgame.bat
@@ -1,11 +1,12 @@
 @echo off
+chcp 65001 > nul
 set JDK_HOME=C:\Users\Administrator\.jdks\ms-11.0.28
 set JAVA=%JDK_HOME%\bin\java.exe
 
 echo Starting zgame server...
 echo Using JDK: %JDK_HOME%
 
-%JAVA% -cp "zgame.jar;config/*;libs/*;" org.game.GameStartUp
+%JAVA% -Dfile.encoding=UTF-8 -cp "zgame.jar;config/*;libs/*;" org.game.GameStartUp
 
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to start zgame server.
