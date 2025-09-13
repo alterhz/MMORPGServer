36224c2fe79d982ed4618432175c9880c12ea5b5
alterhz <550360139@qq.com>
2025-09-13 16:52:05 +0800

    给代码生成注解

---
+++ server/dist/GenProtoIds.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001 > nul
+set JDK_HOME=C:\Users\Administrator\.jdks\ms-11.0.28
+set JAVA=%JDK_HOME%\bin\java.exe
+
+echo Starting zgame server...
+echo Using JDK: %JDK_HOME%
+
+%JAVA% -Dfile.encoding=UTF-8 -cp "zgame.jar;config/*;libs/*;" org.game.tool.ProtoAnnotationAdder "../../proto/json/ProtoIds.ini" "../zgame/src/main/java/org/game/proto"
+
+if %ERRORLEVEL% NEQ 0 (
+    echo Failed to start zgame server.
+    pause
+)
