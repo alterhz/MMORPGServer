b68608a09c64a4645579966e01f0793cf60cef4a
alterhz <550360139@qq.com>
2025-09-13 16:38:17 +0800

    客户端代码生成

--- proto/GenCSharp.bat
+++ proto/GenCSharp.bat
@@ -2,30 +2,38 @@
 setlocal enabledelayedexpansion
 
 set PROTO_DIR=json
-set CSHARP_OUT_DIR=csharp
+set JAVA_OUT_DIR=..\client\zgame\Assets\Scripts\Proto
 
+:: 遍历 PROTO_DIR 下的每个子文件夹
 for /D %%F in (%PROTO_DIR%\*) do (
     set "FOLDER=%%~nxF"
-    set "PACKAGE=!FOLDER: =_!"
-    set "PACKAGE=!PACKAGE!"
-    
-    echo Generating code for !FOLDER!...
-    
-    if not exist "!CSHARP_OUT_DIR!\" (
-        mkdir "!CSHARP_OUT_DIR!"
-    )
-    
-	call quicktype -l csharp --array-type list --features just-types --namespace ZGame --src "!PROTO_DIR!\!FOLDER!" -o "!CSHARP_OUT_DIR!\!FOLDER!.cs"
-	
-    if !errorlevel! equ 0 (
-        echo ? Successfully generated !FOLDER!
-    ) else (
-        echo ? Failed to generate !FOLDER!
+    set "PACKAGE=!FOLDER: =_!"  # 替换空格为下划线
+
+    echo Processing folder: !FOLDER!
+
+    :: 遍历子文件夹中的每个 .json 文件
+    for %%J in ("%%F\*.json") do (
+        set "JSON_FILE=%%~nJ"          # 获取文件名（无扩展名）
+        set "OUTPUT_DIR=!JAVA_OUT_DIR!\!FOLDER!"
+        set "OUTPUT_FILE=!OUTPUT_DIR!\!JSON_FILE!.cs"
+        set "FULL_JSON_PATH=%%J"
+
+        echo Generating !JSON_FILE!.java from %%J ...
+
+        :: 创建输出目录（如果不存在）
+        if not exist "!OUTPUT_DIR!" (
+            mkdir "!OUTPUT_DIR!"
+        )
+		
+		call quicktype -l csharp --array-type list --features just-types --namespace ZGame --src "!FULL_JSON_PATH!" -o "!OUTPUT_FILE!"
+
+        if !errorlevel! equ 0 (
+            echo ? Successfully generated !JSON_FILE!.java
+        ) else (
+            echo ? Failed to generate !JSON_FILE!.java
+        )
     )
 )
 
-echo All proto files processed!
+echo All JSON files processed!
 pause
-
-
-REM quicktype -l csharp --array-type list --features just-types --namespace org.game.proto.login --src Proto/Login/ -o csharp/Login.cs
